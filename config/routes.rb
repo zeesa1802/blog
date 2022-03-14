ca9a728a3328ac0c6ca4360bb081ebfe0475328b
zeesa1802 <zeesa18.2@gmail.com>
2022-03-14 17:25:06 +0500

    Added comment model and associate it to the article model

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-  resources :articles
+  resources :articles do
+    resources :comments
+  end
   root 'welcome#home'
   get 'about', to: 'welcome#about'
   # Defines the root path route ("/")
